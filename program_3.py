930d9ffa997ca0dcd5cbde4022c8efff252c8970
Park1423 <147210472+Park1423@users.noreply.github.com>
2025-09-11 14:17:50 -0500

    Update program_3.py

--- program_3.py
+++ program_3.py
@@ -1,8 +1,17 @@
-def calculate_total_purchase():
-    # A customer in a store is purchasing five items.  
-    # Write a program that asks for each item, 
-    # then displays the subtotal of the sale, 
-    # the amount of sales tax, and the total.  
-    # Assume the sales tax is 7 percent.
+    #figure out how many items we are buying and save that number. I know this isnt required, but I wanted 
+    #the challenge. If you dont like it, you can just enter 5
+print("How many items are you purchasing?")
+numberofitems = int(input())
 
-calculate_total_purchase()
+currentitem = 1
+totalitemcost = 0
+
+    
+while currentitem <= numberofitems:                                    #check to make sure we are still on an item number that we want. If we only have 6 items but we are on item 7, then we shouldnt ask for the price of item 7 and should instead continue on to print our total.
+    print("How much does item number " + str(currentitem) +" cost?")    
+    totalitemcost += float(input())                                    #here, instead of saving the input to a seperate variable and adding it, we simply add the number to the total.
+    currentitem += 1                                                   #here we increase our item number by 1
+else:  
+    print("Item subtotal: " + str(totalitemcost))                      #print out totals, doing the math within the print line to reduce the amount of variables we need.
+    print("Sales tax: " + str(totalitemcost * 0.07))
+    print("     Total: " + str(totalitemcost * 1.07))
